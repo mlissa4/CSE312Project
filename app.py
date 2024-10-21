ca1cfffeb615cc78764c198303155cbcb37710b0
Rohin Kumar <76756199+MayoMan7@users.noreply.github.com>
2024-10-21 19:46:18 -0400

    Day 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,28 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
+@app.route('/login_page')
+def login_page():
     return render_template('login.html')
+
+# RICO AND ERIC PLEASE USE SEPPERATE FILES FOR LOGGING IN AND REGISTERING
+# TRY TO USE SOME LIBARIES!!!
+# Redirect User to home screen on sucessfull login
+
+@app.route('/register', methods=["POST"])
+def register():
+    print("registering")
+    print("Form Data:", request.form)      # For form data (if the request is from a form submission)
+    
+    return "Received!"
+
+@app.route('/login', methods=["POST"])
+def login():
+    print("logging in")
+    print("Form Data:", request.form)      # For form data (if the request is from a form submission)
+    
+    return "Received!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
