6e3e679dcd5b19247374bc9fc053bd5147d2cf0c
Rico C <143468977+ricoc236@users.noreply.github.com>
2024-10-23 15:19:07 -0400

    Delete backend directory

--- backend/models/Users.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true },  // This will store the hashed password
-});
-
-module.exports = mongoose.model('User', userSchema);
