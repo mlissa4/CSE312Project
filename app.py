e00594d4c0a8b408592774aea6da652e625dc9fa
Rico C <143468977+ricoc236@users.noreply.github.com>
2024-10-26 00:46:18 -0400

    added auth token

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask, render_template, jsonify, request, redirect, Response
 from flask_security import UserMixin, RoleMixin, Security, MongoEngineUserDatastore
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from mongoengine import connect, Document
 from utils.auth import *
 from flask_security.utils import hash_password, verify_password
 import os
+import secrets
+import hashlib
 from mongoengine.fields import (
     BinaryField,
     BooleanField,
@@ -15,15 +18,20 @@ from mongoengine.fields import (
     StringField,
 )
 
+
+mongo_clinet = MongoClient('mongo')
+db = mongo_clinet["user_auth"]
+auth= db["auth"]
+
 app = Flask(__name__)
 
 connect('user_auth', host='mongo', port=27017) #path is user_auth
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/user_auth' #go into user_auth collection
-app.config["SECRET_KEY"] = "TEMP_KEY" #scecret key 
-app.config["SECURITY_PASSWORD_SALT"] = "THIS SALT IS REDUNDANe" #Salt here is a seond layer of protect along with the first layer of salt 
+app.config["SECRET_KEY"] = os.getenv("secret_key") #scecret key 
+app.config["SECURITY_PASSWORD_SALT"] = os.getenv("salt") #Salt here is a seond layer of protect along with the first layer of salt 
 
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# UPLOAD_FOLDER = 'static/uploads'
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mongo = PyMongo(app)
 
@@ -49,9 +57,12 @@ def login():
     # if not bcrpyt.check_password_hash(user.password, password):
     if not verify_password(password, user.password):
         return jsonify({"error": "Invalid username or password"}), 404
-
-    return redirect("/",code=302)
-    # return jsonify({"correct": "pass"}), 200
+    auth_token = secrets.token_hex(32)
+    hash_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
+    auth.insert_one({"username": username, "auth_token": hash_auth_token})
+    response = Response(status=302,headers={"Location":"/"})
+    response.set_cookie("auth_token",auth_token,httponly=True, max_age=360000)
+    return response
 
 Security = Security(app,user_datastore)
 @app.after_request
@@ -68,8 +79,7 @@ def home():
     image_files = [f for f in files_in_upload_folder if f.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
 
     # Pass the image filenames to the template
-    return render_template('index.html', images=image_files)
-
+    return render_template('index.html', images=image_files) 
 @app.route('/login_page')
 def login_page():
     return render_template('login.html'), 200
@@ -92,7 +102,6 @@ def register():
 
     return redirect("/login_page",code=302)
 
-    # return f"{password}" #for testing should be removed 
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
@@ -114,7 +123,7 @@ def upload_image():
         for file in files_in_upload_folder:
             print(file)
 
-        # Redirect back to the homepage or another route
+            #Redirect back to the homepage or another route
         return redirect("/", code=302)
     else:
         return 'Error: Image and description are required!'
@@ -129,11 +138,11 @@ def post_redirect():
     return render_template('post_screen.html'), 200
     return redirect("/post_screen",code=302)
 
-# #need to build post_screen.html
-# @app.route('/post_screen')
-# def post_screen():
+#need to build post_screen.html
+@app.route('/post_screen')
+def post_screen():
 
-#     return render_template('post_screen.html'), 200
+    return render_template('post_screen.html'), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
